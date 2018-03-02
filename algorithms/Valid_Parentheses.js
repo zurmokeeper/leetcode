d6e5b1e6f10c969a72142b88d847d173b48a74a1
zurmokeeper <3382272560@qq.com>
2018-03-02 11:25:47 +0800

    加入其它的答案

--- algorithms/Valid_Parentheses.js
+++ algorithms/Valid_Parentheses.js
@@ -2,6 +2,7 @@
  * @param {string} s
  * @return {boolean}
  */
+// The first answer
 var isValid = function(s) {
     if (s.length % 2 === 1 || /[\}\]\)]/.test(s[0])) {
         return false;
@@ -12,7 +13,7 @@ var isValid = function(s) {
             stack.push(s[i]);
         } else {
             var len = stack.length;
-            if (Math.abs(stack[len - 1].charCodeAt(0) - s[i].charCodeAt(0)) <= 2) {
+            if (Math.abs(stack[len - 1].charCodeAt(0) - s[i].charCodeAt(0)) <= 2) {   //通过字符的unicode编码的值来判断
                 stack.pop();
             } else {
                 return false;
@@ -22,6 +23,32 @@ var isValid = function(s) {
     if (stack.length === 0) {
         return true;
     } else {
-        return false;
+        return false;       //比如"(("   只有左边的部分
     }
 };
+
+//The second answer
+
+// var map = {
+//     "(": ")",
+//     "[": "]",
+//     "{": "}"
+// } 
+
+// var isValid = function(s) {
+//     var stack = [];
+    
+//     for (var i = 0; i < s.length; i++) {
+//         var el = s[i];
+        
+//         if (map[el]) {
+//             stack.push(map[el]);
+//         } else {
+//             if (el !== stack.pop()) {
+//                 return false;
+//             }
+//         }
+//     }
+    
+//     return stack.length === 0;
+// };
